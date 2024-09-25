1588ad52efcca05c9026d936193391d5450b2c27
MariaAlicePrado <mariaalice.ap2006@gmail.com>
2024-09-25 07:16:32 -0300

    primeiro commit

--- src/model/Professor.java
+++ src/model/Professor.java
@@ -1,9 +1,43 @@
 package model;
 
+
+
 public class Professor{
 
     public Long rm;
     public Long senha;
     public String nome;
     public boolean cod_status;
+
+    public Long getRm() {
+        return rm;
+    }
+
+    public void setRm(Long rm) {
+        this.rm = rm;
+    }
+
+    public Long getSenha() {
+        return senha;
+    }
+
+    public void setSenha(Long senha) {
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public boolean isCod_status() {
+        return cod_status;
+    }
+
+    public void setCod_status(boolean cod_status) {
+        this.cod_status = cod_status;
+    }
 }
